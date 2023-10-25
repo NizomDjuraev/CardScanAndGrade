c27215c4323cb4afc4c477eb9f69cbe5bfa4c3e2
Wyatt Kaiser <wyatt.kaiser@outlook.com>
2023-10-24 22:10:01 -0400

    Sign Up/Sign in Email and Google
    - sign up screen with email password
    - sign in with email password
    - sign in with google

---
+++ app.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'ReactNativeApp',
+    version: '1.0.0',
+    extra: {
+      clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    },
+  };
--- app/index.js
+++ app/index.js
@@ -1,11 +1,51 @@
-import {View, Text} from 'react-native';
+import React from "react";
+import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import Constants from "expo-constants"
+import SignUpScreen from "./components/SignUpScreen";
+import SignInWithEmail from "./components/SignInWithEmail";
+import SignInWithGoogle from "./components/SignInWithGoogle";
+import * as SecureStore from "expo-secure-store";
 
-const Home = () => {
-    return (
-        <View>
-            <Text>Landing Page</Text>
-        </View>
-    )
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key)
+    } catch (err) {
+      return null
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value)
+    } catch (err) {
+      return
+    }
+  }
+}
+ 
+const App = () => {
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={Constants.expoConfig.extra.clerkPublishableKey}>
+      <SafeAreaView style={styles.container}>
+        <SignedIn>
+            <Text>You are signed in!</Text>
+        </SignedIn>
+        <SignedOut>
+            <SignInWithGoogle />
+        </SignedOut>
+      </SafeAreaView>
+    </ClerkProvider>
+  );
 }
 
-export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default App;
