b6f707dafe2e869c38f1c0d4dacd37d657fdcac1
Nizom Djuraev <nd596@drexel.edu>
2023-11-15 12:38:49 -0500

    CameraScreen and adjust borders screen Update
    Updated CameraScreen and started the adjust borders screen. Manually implementing, the packages I found were incompatible with react native in expo.

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,13 +1,41 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, PanResponder } from 'react-native';
 import { Camera } from 'expo-camera';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'; 
 import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
+
 
 export default function CameraScreen() {
   const [hasPermission, setHasPermission] = useState(null);
   const [capturedPhoto, setCapturedPhoto] = useState(null);
   const cameraRef = useRef(null);
+  const [corners, setCorners] = useState({
+    topLeft: { x: 40, y: 110 },
+    topRight: { x: 40, y: 110 },
+    bottomLeft: { x: 40, y: 110 },
+    bottomRight: { x: 40, y: 110 },
+  });
+  
+  useEffect(() => {
+    const createPanResponder = (cornerName) => PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gestureState) => {
+        const newCorners = { ...corners };
+        newCorners[cornerName] = {
+          x: newCorners[cornerName].x + gestureState.dx,
+          y: newCorners[cornerName].y + gestureState.dy,
+        };
+        setCorners(newCorners);
+      },
+    });
+  
+    const topLeftResponder = createPanResponder('topLeft');
+    const topRightResponder = createPanResponder('topRight');
+    const bottomLeftResponder = createPanResponder('bottomLeft');
+    const bottomRightResponder = createPanResponder('bottomRight');
+  
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -19,9 +47,15 @@ export default function CameraScreen() {
   const takePicture = async () => {
     if (cameraRef.current) {
       const photo = await cameraRef.current.takePictureAsync();
-      setCapturedPhoto(photo.uri);
+      const croppedPhoto = await ImageManipulator.manipulateAsync(
+        photo.uri,
+        [{ crop: { originX: 0, originY: 0, width: photo.width, height: photo.height } }],
+        { compress: 1, format: ImageManipulator.SaveFormat.PNG }
+      );
+      setCapturedPhoto(croppedPhoto.uri);
     }
   };
+  
 
   const retakePicture = () => {
     setCapturedPhoto(null);
@@ -57,12 +91,35 @@ export default function CameraScreen() {
       {capturedPhoto ? (
         <View style={styles.previewContainer}>
           <Image source={{ uri: capturedPhoto }} style={styles.previewImage} />
+        <TouchableOpacity
+          style={[styles.corner, { top: corners.topLeft.y, left: corners.topLeft.x }]}
+          // {...topLeftResponder.panHandlers}
+        />
+        <TouchableOpacity
+          style={[styles.corner, { top: corners.topRight.y, right: corners.topRight.x }]}
+          // {...topRightResponder.panHandlers}
+        />
+        <TouchableOpacity
+          style={[styles.corner, { bottom: corners.bottomLeft.y, left: corners.bottomLeft.x }]}
+          // {...bottomLeftResponder.panHandlers}
+        />
+        <TouchableOpacity
+          style={[styles.corner, { bottom: corners.bottomRight.y, right: corners.bottomRight.x }]}
+          // {...bottomRightResponder.panHandlers}
+        />
           <TouchableOpacity onPress={retakePicture} style={styles.closeButton}>
             <Text style={styles.closeButtonText}>X</Text>
           </TouchableOpacity>
         </View>
       ) : (
         <Camera style={styles.camera} ref={cameraRef}>
+          <View style={styles.overlay} />
+          <View style={styles.rectangleContainer}>
+            <View style={styles.tab}>
+              <Text style={styles.tabText}>Front of Card</Text>
+            </View>
+            <View style={styles.rectangle} />
+          </View>
           <View style={styles.cameraFooter}>
             <TouchableOpacity onPress={openCameraRoll} style={styles.iconButton}>
               <MaterialIcons name="insert-photo" size={24} color="white" />
@@ -140,5 +197,49 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  rectangle: {
+    position: 'absolute',
+    alignSelf: 'center',
+    height: '70%',
+    width: '80%',
+    borderWidth: 2,
+    borderColor: 'white',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  tab: {
+    width: '50%',
+    backgroundColor: 'black',
+    padding: 4,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    transform: [{ translateY: 90 }],
+  },
+  tabText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  rectangleContainer: {
+    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+  },
+  corner: {
+    position: 'absolute',
+    width: 10,
+    height: 10,
+    backgroundColor: 'red', // A bright color for visibility
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1, // Ensure it's above other elements
   },
 });
