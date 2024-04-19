63d2e607f7d62b3cedd21fdb095031ba10483638
Wyatt Kaiser <wyatt.kaiser@outlook.com>
2024-04-18 23:03:15 -0400

    Forgot Password Screen Complete
    forgot password screen that the user is brought to after clicking forgot password on the sign in screen. sends an email to reset and brings user back to sign in after

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -1,13 +1,54 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+} from "react-native";
+import { auth } from "../firebaseConfig";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { useNavigation } from "@react-navigation/native";
 
 export default function ForgotPasswordScreen() {
+  const [email, setEmail] = useState("");
+  const navigation = useNavigation();
+
+  const handleResetPassword = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        Alert.alert(
+          "Password Reset Email Sent",
+          "Please check your email to reset your password.",
+          [
+            {
+              text: "OK",
+              onPress: () => navigation.navigate("SignIn"),
+            },
+          ]
+        );
+      })
+      .catch((error) => {
+        Alert.alert("Error", error.message);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Forgot Password</Text>
       <Text style={styles.description}>
         Enter your email address to reset your password.
       </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+        value={email}
+        autoCapitalize="none"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleResetPassword}>
+        <Text style={styles.buttonText}>Reset Password</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,18 +56,37 @@ export default function ForgotPasswordScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: "#fff",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
+    backgroundColor: "#272727",
+    padding: 30,
   },
   description: {
     fontSize: 16,
+    color: "#fff",
     textAlign: "center",
     marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  input: {
+    color: "#fff",
+    width: "80%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#1D9DB9",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
