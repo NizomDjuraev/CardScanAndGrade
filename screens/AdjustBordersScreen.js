59dd49d103fbf57aecb94e01560ed1790f78515f
cdh-94 <122650002+cdh-94@users.noreply.github.com>
2024-04-22 23:21:58 -0400

    Update AdjustBordersScreen.js

--- screens/AdjustBordersScreen.js
+++ screens/AdjustBordersScreen.js
@@ -13,8 +13,8 @@ import { useRoute } from "@react-navigation/native"; // Import useRoute
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-const imageWidth = 300;
-const imageHeight = 300;
+const imageWidth = 280;
+const imageHeight = 280;
 const imageX = (windowWidth - imageWidth) / 2;
 const imageY = (windowHeight - imageHeight) / 2;
 
